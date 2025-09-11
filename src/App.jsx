03de106a12acf4d57e9d9798c99900070eabda25
pradeep kumar rana <pkr86037@gmail.com>
2025-09-11 13:45:32 +0530

    new UI and code optimize

--- src/App.jsx
+++ src/App.jsx
@@ -1,135 +1,86 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import "./App.css"
 import { FaCheck } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-import "./App.css";
 
-export default function App() {
-  const [inputValue, setinputValue] = useState("");
+export const App = () => {
+  const [dateTime, setDateTime] = useState();
+  const [inputValue, setInputValue] = useState("");
   const [task, setTask] = useState(() => {
-    const rawTodo = localStorage.getItem("todoKey");
-    if (!rawTodo) return [];
-    return JSON.parse(rawTodo);
+  const rowTodo =  localStorage.getItem('todoKey');
+  return rowTodo ? JSON.parse(rowTodo) : []; 
   });
-  const [dateTime, SetDateTime] = useState("");
-  // todo Add data to local storage
-  localStorage.setItem("todoKey", JSON.stringify(task));
 
+localStorage.setItem("todoKey", JSON.stringify(task));
+
+  // todo App Time and date show
 useEffect(() => {
-  const interval = setInterval(() => {
-    const newDate = new Date().toDateString();
-    const newTime = new Date().toLocaleTimeString("en-US",{
-      hour:"numeric",
-      minute:"numeric",
-      second:"numeric",
-      hour12:true,
-    })  
-    SetDateTime(`${newDate} - ${newTime}`)
-  });
-  return () => clearInterval(interval);
+ const interval = setInterval(() => {
+    const newDate = new Date().toLocaleDateString();
+    const newTime = new Date().toLocaleTimeString("en-US");
+    setDateTime(`${newDate}-${newTime}`);
+  }, 1000);
+return () => clearInterval(interval)
 },[])
 
-  const handleInputChange = (value) => {
-    setinputValue(value);
-  };
-
-  const handleFormSubmit = (e) => {
+  const submitFormEvent = (e) => {
     e.preventDefault();
-
-    if (!inputValue) return;
-
-    if (task.some((t) => t.text === inputValue)) {
-      setinputValue("");
+   
+    const trimmedValue = inputValue.trim();
+    if(!trimmedValue){
+      setInputValue("");
       return;
+    };
+  
+    if(task.some((t) => t.text === trimmedValue)){
+        setInputValue("");
+        return;
     }
+    setTask((prev) => [...prev,{text:trimmedValue,completed:false}]);
+    setInputValue("");
+  }
+   
+  const removeElement = (value) => {
+    const updateRemove = task.filter((remove) => remove.text !== value)
+     setTask(updateRemove)
+  }
+ 
+  const checkUncheck = (value) => {
+     const updatedTask =    task.map((t) => t.text ===value ? {...t,completed: !t.completed}:t);
+     setTask(updatedTask)
+  }
 
-    setTask((prevTask) => [
-      ...prevTask,
-      { text: inputValue, completed: false },
-    ]);
 
-    setinputValue("");
-  };
-
-  const handleDeleteElement = (value) => {
-    const updatedFunction = task.filter((curr) => curr.text !== value);
-    setTask(updatedFunction);
-  };
-
-  const handleToggleComplete = (value) => {
-    const updatedTasks = task.map((t) =>
-      t.text === value ? { ...t, completed: !t.completed } : t
-    );
-    setTask(updatedTasks);
-  };
-
-  const clearAllList = () => {
+  const clearAll = () => {
     setTask([]);
-  };
-
-  return (
-    <>
-      <section className="todo-cantaner">
-        <header>
-          <h1>Todo List</h1>
-        </header>
-        <div className="sate">
-          <h2>{dateTime}</h2>
-        </div>
+  }
+  return ( 
+    <div><section>
+      <header>
+        <h1>Todo List App</h1>
+      </header>
+      <div className="timezone">{dateTime}</div>
       </section>
-
-      <section className="form">
-        <form onSubmit={handleFormSubmit}>
-          <div>
-            <input
-              type="text"
-              className="todo-input"
-              autoComplete="off"
-              value={inputValue}
-              onChange={(e) => handleInputChange(e.target.value)}
-            />
-          </div>
-          <button type="submit">Add Task</button>
-        </form>
-
-        <section className="display-section">
-          <ul>
-            {task.map((list, index) => (
-              <li className="list-cantaner" key={index}>
-                <div className="result">
-                  <div
-                    className={`result-name ${ list.completed ? "completed-task" : "" }`}>
-                    {list.text}
-                  </div>
-                  <div className="icons">
-                    <div className="thik-icon">
-                      <button
-                        className="checkBtn"
-                        onClick={() => handleToggleComplete(list.text)}
-                      >
-                        <FaCheck/>
-                      </button>
-                    </div>
-                    <div className="del-icon">
-                      <button
-                        className="deleteBtn"
-                        onClick={() => {
-                          handleDeleteElement(list.text);
-                        }}
-                      >
-                        <MdDelete />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-
-          <button type="button" className="clear-btn" onClick={clearAllList}>
-            Clear All
-          </button>
-        </section>
-      </section>
-    </>
+      <form onSubmit={submitFormEvent}> 
+        <input type="text" placeholder="Add Task" value={inputValue} onChange={(e) => setInputValue(e.target.value)}  />
+        <button type="submit">Add</button>
+      </form>
+      <div>
+        <ul type="circle">
+            {
+                task.map((curr,index) => {
+                    return <li key={index}>
+                        <div className="list-div">
+                            <p className={` display-item ${curr.completed ? "completed-task" : ""}`}>{curr.text}</p>
+                        </div>
+                        <button onClick={() => checkUncheck(curr.text)}><FaCheck /></button>
+                        <button onClick={() => removeElement(curr.text)}><MdDelete /></button>
+                    </li> 
+                })
+            }
+        </ul>
+        <button onClick={clearAll}>Clear All</button>
+      </div>
+    </div>
   );
-}
+};
